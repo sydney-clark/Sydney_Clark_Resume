97f7afb9a4b806aeaa1e9e579690e3493b930172
sydney-clark <sydney.natasha.clark@gmail.com>
2023-02-08 15:14:17 -0800

    added Harry Potter carousel

--- app.js
+++ app.js
@@ -3,10 +3,54 @@
 $(() => { //creating the DOM query
 
 
+//     const $navBtn = $('.navBar')
+// $navBtn.on('click', function () {
+//     $('.nav').css('display', 'block')
+//     $('.nav').on('click', function () {
+//         $('.nav').css("display", 'none')
+//     })
+// })
+
+// $(window).scroll(function () {
+//     if (window.scrollY > 30) {
+//         $navBtn.css("opacity", "0.6")
+//     } else (
+//         $navBtn.css("opacity", "1")
+//     )
+// })
 
 
+let currentImgIndex = 0;
+let numOfImages = $('.carousel-images').children().length - 1;
 
+$('.next').on('click', () => {
+    $('.carousel-images').children().eq(currentImgIndex).css('display', 'none');
 
+    if (currentImgIndex < numOfImages) {
+        currentImgIndex++;
+    } else {
+        currentImgIndex = 0;
+    }
 
+    $('.carousel-images')
+        .children()
+        .eq(currentImgIndex)
+        .css('display', 'block');
+
+});
+$('.previous').on('click', () => {
+    $('.carousel-images').children().eq(currentImgIndex).css('display', 'none');
+    if (currentImgIndex > 0) {
+        currentImgIndex--;
+    } else {
+        currentImgIndex = numOfImages;
+    }
+
+    $('.carousel-images')
+        .children()
+        .eq(currentImgIndex)
+        .css('display', 'block');
+
+});
 
 });
